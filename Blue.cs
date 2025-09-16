db782e9370fa44acdfbdf96db15f2201954049e9
Margaryan-Armina-Arsenovna <armina2407@mail.ru>
2025-09-16 11:45:24 +0300

    Маргарян Армина Арсеновна БИВТ-25-16 синяя лига

---
+++ Blue.cs
@@ -0,0 +1,143 @@
+﻿namespace Lab1
+{
+    public class Blue
+    {
+        public bool Task1(int a, int b)
+        {
+            bool answer = false;
+
+            // code here
+            if (a > 0 && b > 0 || a < 0 && b < 0)
+            {
+                answer = true;
+            }
+            // end
+
+            return answer;
+        }
+        public bool Task2(double d)
+        {
+            bool answer = false;
+
+            // code here
+            int a = (int)d;
+            double res = d - a;
+            double ans = Math.Round(res, 4);
+            if (ans != 0)
+            {
+                answer = true;
+            }
+            // end
+
+            return answer;
+        }
+        public bool Task3(int a, int b)
+        {
+            bool answer = false;
+
+            // code here
+            if (b != 0 && a % b == 0)
+            {
+                answer = true;
+            }
+            // end
+
+            return answer;
+        }
+        public double Task4(double d, double f, double g)
+        {
+            double answer = 0;
+
+            // code here
+            double d1 = Math.Abs(d);
+            double f1 = Math.Abs(f);
+            double g1 = Math.Abs(g);
+            if (d1 >= f1 && d1 >= g1)
+            {
+                answer = d;
+            }
+            else if (f1 >= d1 && f1 >= g1)
+            {
+                answer = f;
+            }
+            else
+            {
+                answer = g;
+            }
+            // end
+
+            return answer;
+        }
+        public double Task5(double x)
+        {
+            double answer = 0;
+
+            // code here
+            if (x <= -1)
+            {
+                answer = 0;
+            }
+            else if (x <= 0)
+            {
+                answer = x + 1;
+            }
+            else
+            {
+                answer = 1;
+            }
+            // end
+
+            return answer;
+        }
+        public bool Task6(double circleS, double squareS)
+        {
+            bool answer = false;
+
+            // code here
+            if (circleS / Math.PI * 4 <= squareS)
+            {
+                answer = true;
+            }
+            // end
+
+            return answer;
+        }
+
+        public double Task7(double d, double f)
+        {
+            int answer = 0;
+
+            // code here
+            if (Math.Abs(d) < Math.Abs(f))
+            {
+                if (d > 0)
+                {
+                    answer = -1;
+                }
+            }
+            else
+            {
+                if (f > 0)
+                {
+                    answer = 1;
+                }
+            }
+            // end
+
+            return answer;
+        }
+        public bool Task8(int a, int b, int c)
+        {
+            bool answer = false;
+
+            // code here
+            if (a / 2 + b / 2 + c / 2 == 2 || (a / 2 + b / 2 + c / 2 > 2 && (a / 2 + b / 2 + c / 2) % 3 != 1))
+            {
+                answer = true;
+            }
+            // end
+
+            return answer;
+        }
+    }
+}
